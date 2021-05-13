039a85914397b85e9cce90f6c9755c583e6a0bcf
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-05-13 12:47:46 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,43 +1,59 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
  
 class App extends React.Component {
     // eslint-disable-next-line no-useless-constructor
-    constructor(props){
-        super(props);
+    // constructor(props){
+    //     super(props);
         
-        //This is the only time we do direct assignment to this.state
-        this.state = {
-            lat:null,
-            errorMessage:'',
-        };
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setstate
-                this.setState({lat:position.coords.latitude})
-            },
-            (err) =>  {
-                this.setState({errorMessage:err.message})
-            }
-        );
+    //     //This is the only time we do direct assignment to this.state
+    //     this.state = {
+    //         lat:null,
+    //         errorMessage:'',
+    //     };
+       
 
+    // }
+      
+    state = {lat:null,errorMessage:''};
+
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({lat:position.coords.latitude}),
+            (err) => this.setState({errorMessage:err.message})
+        );
     }
+    componentDidUpdate(){
+        console.log('My component was just updated - it rerendered!');
+    }
+
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return (
+                <div>Error:{this.state.errorMessage}</div>
+            )
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return(
+                 <SeasonDisplay lat = {this.state.lat}/>
+            )
+        }
+        return <Spinner message="Accept accept location request"/> 
+    }
+
 
 
     //React says we have to define render !!
     render(){
-       if(this.state.errorMessage && !this.state.lat){
-           return (
-               <div>Error:{this.state.errorMessage}</div>
-           )
-       }
-       if(!this.state.errorMessage && this.state.lat){
-           return(
-               <div>Latitude : {this.state.lat}</div>
-           )
-       }
-       return <div>Loading !!!</div>
+      return (
+          <div className="border red">
+              {this.renderContent()}
+          </div>
+      )
     }
 }
 
